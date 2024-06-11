01d60d8531c0459482359052f84f4cf583d8ee07
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-06-11 11:51:31 +0200

    feat(02-intro-vue): Use v-for

--- vue/02-intro-vue/app.js
+++ vue/02-intro-vue/app.js
@@ -1,35 +1,38 @@
-console.log(Vue);
-
 const { createApp, ref } = Vue;
 
+const quotes = [
+  {
+    quote:
+      "The night is darkest just before the dawn. And I promise you, the dawn is coming.",
+    author: "Harvey Dent, The Dark Knight",
+  },
+  {
+    quote: "I believe what doesn’t kill you simply makes you, stranger.",
+    author: "The Joker, The Dark Knight",
+  },
+  {
+    quote:
+      "Your anger gives you great power. But if you let it, it will destroy you… As it almost did me",
+    author: "Henri Ducard, Batman Begins",
+  },
+  {
+    quote:
+      "You either die a hero or live long enough to see yourself become the villain.",
+    author: "Harvey Dent, The Dark Knight",
+  },
+  {
+    quote: "If you’re good at something, never do it for free.",
+    author: "The Joker, The Dark Knight",
+  },
+  {
+    quote: "Yes, father. I shall become a bat.",
+    author: "Bruce Wayne/Batman, Batman: Year One",
+  },
+];
+
 const app = createApp({
-  /* template: `
-    <h1>Hello World</h1>
-    <h2>{{message}}</h2>
-    <h2>{{messageReactive}}</h2>
-    <h2>{{author}}</h2>
-    <p>From app.js</p>
-    `, */
-
   setup() {
-    console.log("setup");
-
-    const message = "Example message";
-    const messageReactive = ref("Example message");
-
-    const author = ref("John Doe");
-
-    /* setTimeout(() => {
-      messageReactive.value = "New message";
-      author.value = "Jane Doen";
-    }, 1500); */
-
-    const changeMessage = () => {
-      messageReactive.value = "New message";
-      author.value = "Jane Doen";
-    };
-
-    return { message, messageReactive, author, changeMessage };
+    return { quotes };
   },
 });
 
