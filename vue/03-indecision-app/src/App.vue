f66c395ec3be7762877b1e59bbf8587542cb8cde
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-06-13 16:56:50 +0200

    feat(03-indecision-app): Handle state & events

--- vue/03-indecision-app/src/App.vue
+++ vue/03-indecision-app/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-    <h1>Hello world !!!</h1>
+    <section>
+        <h3>Counter: {{counter}}</h3>
+        <h3>Square: {{squareCounter}}</h3>
+    
+        <div>
+            <button @click="counter++">+1</button>
+            <button @click="counter--">-1</button>
+        </div>
+    </section>
 </template>
 
 <script lang="ts" setup>
-    console.log('Hello world')
+    import { computed, ref } from 'vue';
+
+    const counter = ref(10);
+    const squareCounter = computed(() => Math.pow(counter.value, 2));
+
 </script>
 
 <style scoped> /* scoped --> applies only to this component */
