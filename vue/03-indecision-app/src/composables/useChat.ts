dbe4d6053d0163e6d9c684c3ce1d415b980f0df8
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-06-19 17:05:35 +0200

    feat(03-indecision-app): Handle http request

--- vue/03-indecision-app/src/composables/useChat.ts
+++ vue/03-indecision-app/src/composables/useChat.ts
@@ -1,15 +1,35 @@
 import type { ChatMessage } from '@/interfaces/chat-message.interface';
 import { ref } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
+import type { YesNoResponse } from '@/interfaces/yes-no-response';
+import { sleep } from '@/helpers/sleep';
 
 export const useChat = () => {
   const messages = ref<ChatMessage[]>([]);
 
-  const onMessage = (text: string) => {
+  const getApiResponse = async (): Promise<YesNoResponse> => {
+    const resp = await fetch('https://yesno.wtf/api');
+    return (await resp.json()) as YesNoResponse;
+  };
+
+  const onMessage = async (text: string) => {
+    if (text.length === 0) return;
+
     messages.value.push({
       id: uuidv4(),
       text,
       isOwn: true
+    });
+
+    if (!text.endsWith('?')) return;
+    await sleep(1.5);
+    const { answer, image } = await getApiResponse();
+
+    messages.value.push({
+      id: uuidv4(),
+      text: answer,
+      isOwn: false,
+      image
     });
   };
 
---
+++ vue/03-indecision-app/src/helpers/sleep.ts
@@ -0,0 +1,5 @@
+export const sleep = (seconds: number = 1): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
